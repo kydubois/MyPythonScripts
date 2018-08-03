2f7bdc0265f570fe2580207263669988e95e544f
kydubois <kydubois28@gmail.com>
2018-08-03 10:50:57 -0700

    Update insightOne.py

--- Insight/master/insightOne.py
+++ Insight/master/insightOne.py
@@ -21,10 +21,7 @@ ctx.verify_mode = ssl.CERT_NONE
 db = sqlite3.connect(tagname + '_Orgs.sqlite')  # Make sure to update to new SQL database
 cur = db.cursor()
 
-# Create a new database
-cur.execute('''
-DROP TABLE IF EXISTS Orgs;''')
-
+# Create a new database table
 cur.execute('''
 CREATE TABLE IF NOT EXISTS Orgs (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -33,15 +30,6 @@ CREATE TABLE IF NOT EXISTS Orgs (
     data TEXT
 );
 ''')
-
-# create a new CSV file
-fhand = open(tagname + '_OrgsList_with_Emails.csv', 'w', newline='')
-csvfhand = csv.writer(fhand)
-csvfhand.writerow(['Hospital','Hospital Email','Hospital ID'])
-
-hosphand = open(tagname + '_OrgsList.csv', 'w', newline='')
-csvhosphand = csv.writer(hosphand)
-csvhosphand.writerow(['Hospital'])
 
 count = 0
 while True:
@@ -80,66 +68,6 @@ while True:
 
     data = uh.json()  # this is a list
     # print(type(data))
-
-    EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
-
-    for item in data:
-        # pp.pprint(item)
-        # print('')
-        hospname = str(item['ORGANISATION_NAME'].encode('utf-8'))
-        hospname = hospname[2:-1]
-        hospID = item['ORGANISATION_ID']
-
-        # print('length of tags ',len(item['TAGS']))
-        taglist = list()
-        for i in item['TAGS']:
-            tag = i['TAG_NAME']
-            # print(tag)
-            taglist.append(tag)
-
-        # print('length of CUSTOMFIELDS ',len(item['CUSTOMFIELDS']))
-        emaillist = list()
-        for r in item["CUSTOMFIELDS"]:
-            if EMAIL_REGEX.match(r["FIELD_VALUE"]):  # search for email, skip phone numbers
-                email = str(r["FIELD_VALUE"].encode('utf-8'))
-                # print(email[2:-1])
-                email = email[2:-1]
-                emaillist.append(email)
-            else:
-                continue
-
-
-        print(hospname)
-        # print(len(taglist))
-        for t in taglist:
-            ttag = t
-            # print(ttag)
-        # print(len(emaillist))
-        for e in emaillist:
-            eemail = e
-            if len(emaillist) == 0:
-                eemail = 'N/A'
-                print(eemail)
-            print(eemail)
-        print('')
-        count = count + 1
-
-        cur.execute('''INSERT INTO Orgs (hospital, hospital_ID, data)
-            VALUES ( ?, ?, ? );''', (str(hospname), str(hospID), str(item) ) )
-        db.commit()
-
-        #write hospital name and email to csv
-        csvfhand.writerow([hospname,emaillist,hospID])
-        csvhosphand.writerow([hospname])
-
-    if count % 10 == 0:
-        print('Compiling Data...')
-        time.sleep(5)
-
-
-    if count == dumps_total:
-        print("Total of",count, "records written to database.")
-        break
 
 cur.close()
 fhand.close()
