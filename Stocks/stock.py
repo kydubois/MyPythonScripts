bfd61fc0251a9f8123a7331f261ba5951ea6a6e4
kydubois <kydubois28@gmail.com>
2020-02-07 22:09:52 -0800

    Update stock.py

--- Stocks/stock.py
+++ Stocks/stock.py
@@ -4,37 +4,25 @@
 Developed by Kyle DuBois. Version 1. Written 2/7/2020'''
 
 from datetime import datetime
-from datetime import date
-
-# Generate dictionaries for stock details.
-stock_0 = {'symbol':'GOOGL', 'no_shares':125, 'purchase_price':772.88, 'current_price': 941.53, 'purchase_date':'8/1/2019'}
-stock_1 = {'symbol':'MSFT', 'no_shares':85, 'purchase_price':56.60, 'current_price':73.04, 'purchase_date':'8/1/2015'}
-stock_2 = {'symbol':'RDS-A', 'no_shares':400, 'purchase_price':49.58, 'current_price':55.74, 'purchase_date':'8/1/2015'}
-stock_3 = {'symbol':'AIG', 'no_shares':235, 'purchase_price':54.21, 'current_price':65.27, 'purchase_date':'8/1/2015'}
-stock_4 = {'symbol':'FB', 'no_shares':150, 'purchase_price':124.31, 'current_price':172.45, 'purchase_date':'8/1/2015'}
-stock_5 = {'symbol':'M', 'no_shares':425, 'purchase_price':30.30, 'current_price':23.98, 'purchase_date':'1/10/2017'}
-stock_6 = {'symbol':'F', 'no_shares':85, 'purchase_price':12.58, 'current_price':10.95, 'purchase_date':'2/17/2017'}
-stock_7 = {'symbol':'IBM', 'no_shares':80, 'purchase_price':150.37, 'current_price':145.30, 'purchase_date':'5/12/2017'}
-
-myStocks = [stock_0, stock_1, stock_2, stock_3, stock_4, stock_5, stock_6, stock_7] 
+from datetime import date 
 
 class Stock:
+    """Simple representation of a stock."""
 
     def __init__(self, symbol, no_shares, purchase_price, current_price, purchase_date):
-        """Represents aspects of a stock."""
+        """Initialize symbol, no_shares, purchase_price, current_price, and purchase_date attribues of a stock."""
         self.symbol = symbol
         self.no_shares = no_shares
         self.purchase_price = purchase_price
         self.current_price = current_price
         self.purchase_date = purchase_date
-        self.stock_list = []
 
     # Create date object
     d1 = date.today()  # Get todays date
     today = d1.strftime("%m/%d/%Y")  # Set todays date in string object. 
 
-    def build_stock(self, symbol, no_shares, purchase_price, current_price, purchase_date): 
-        """Build a new deictionary item for a stock and append it to myList of stocks."""
+    def build_stock(self): 
+        """Build a new dictionary item for a stock."""
         stock = {'symbol':self.symbol, 
                  'no_shares':self.no_shares, 
                  'purchase_price':self.purchase_price, 
@@ -42,9 +30,6 @@ class Stock:
                  'purchase_date':self.purchase_date
                  }
         return stock
-    
-    def set_stock_list(self, stock)
-        self.stock_list.append(stock)
 
     def investment_returns(self):
         """Calculates the earnings or losses for a stock"""
@@ -66,15 +51,17 @@ class Stock:
         return yearlyReturn  # Return yearly return on investment.
 
 class Bond(Stock):
+    """Simple representation of a bond that inherits from a stock."""
 
     def __init__(self, symbol, no_shares, purchase_price, current_price, purchase_date, coupon, bond_yield):
-        """Represents aspects of a stock, specif to a bond."""
+        """Initialize symbol, no_shares, purchase_price, current_price, purchase_date, coupon, and bond_yield attributes of a bond."""
         super().__init__(symbol, no_shares, purchase_price, current_price, purchase_date)
         self.coupon = coupon
         self.bond_yield = bond_yield
     
-    def build_stock(self, symbol, no_shares, purchase_price, current_price, purchase_date, coupon, bond_yield):
-        stock = {'symbol':self.symbol, 
+    def build_stock(self):
+        """Builds a new dictionary item for a bond."""
+        bond = {'symbol':self.symbol, 
             'no_shares':self.no_shares, 
             'purchase_price':self.purchase_price, 
             'current_price':self.current_price, 
@@ -82,40 +69,45 @@ class Bond(Stock):
             'coupon':self.coupon,
             'bond_yield':self.bond_yield
             }
-        self.stock_list.append(stock)
-
-    
+        return bond  
 
 class Investor:
+    """Simple representation of an investor."""
 
     def __init__(self, name, address, phone):
-        self.name = name
+        """Initialize name, address, and phone attributes."""
+        self.name = name.title()
         self.address = address
         self.phone = phone
         self.investor_ID = 1
 
     def get_investor(self):
+        """Returns the investors name"""
         print(f"\nStock ownership for {self.name}")
         print('----------------------------------\n')
 
+# Create list to put the newly created stocks
+investor_holdings = []
 
-new_investor = Investor('Bob Smith', '123 Main St', '123-456-7890')
-new_investor.get_investor()
+# Instantiate all the stocks and one bond class, build a stock dictionary, and appned to stock list
+investor_holdings.append(Stock('GOOGL', 125, 772.88, 941.53, '8/1/2019').build_stock())
+investor_holdings.append(Stock('MSFT', 85, 56.60, 73.04, '8/1/2015').build_stock())
+investor_holdings.append(Stock('RDS-A', 400, 49.58, 55.74, '8/1/2015').build_stock())
+investor_holdings.append(Stock('AIG', 235, 54.21, 65.27, '8/1/2015').build_stock())
+investor_holdings.append(Stock('FB', 150, 124.31, 172.45, '8/1/2015').build_stock())
+investor_holdings.append(Stock('M', 425, 30.30, 23.98, '1/10/2017').build_stock())
+investor_holdings.append(Stock('F', 85, 12.58, 10.95, '2/17/2017').build_stock())
+investor_holdings.append(Stock('IBM', 80, 150.37, 145.30, '5/12/2017').build_stock())
+investor_holdings.append(Bond('GT2:GOV', 200, 100.02, 100.05, '8/1/2017', 1.38, 1.35).build_stock())
+
+new_investor = Investor('Bob Smith', '123 Main St', '123-456-7890')  # Instantiate Investor class
+new_investor.get_investor()  # Call Investor class function to get investor name.
 print('\nSTOCK            SHARE#         EARNINGS/LOSS        YEARLY% EARNINGS/LOSS')
-print('-----------------------------------------------------------------------------------')
-# iterate through myStocks list. 
-# refernce dictionary keys for stock symbol, number of shares, and total value.
-for stock in myStocks:
-    returns = Stock.investment_returns(stock)
-    yearly = Stock.yearly_return(stock)
-    print('{symbol}            {shares}            ${returns:.2f}            {yearly:.2f}%'.format(symbol=stock['symbol'], shares=stock['no_shares'], returns=returns, yearly=yearly))
-
-# print('\nBOND           SHARE#         EARNINGS/LOSS        YEARLY% EARNINGS/LOSS')
-# print('-----------------------------------------------------------------------------------')
-# for bond in myBond;
-
-my_bond = Bond('GT2:GOV', 200, 100.02, 100.05, '8/1/2017', 1.38, 1.35)
-my_bond.build_stock()
-
-for stock in myStocks:
-    print(stock['symbol'])
+print('----------------------------------------------------------------------------')
+for stock in investor_holdings:  # Iterate through investor holdings list. 
+    returns = Stock.investment_returns(stock)  # Call Stock class function to get investment returns.
+    yearly = Stock.yearly_return(stock)  # Call Stock class function to get yearly return percentage.
+    print('{symbol}            {shares}            ${returns:.2f}            {yearly:.2f}%'.format(symbol=stock['symbol'], 
+                                                                                                   shares=stock['no_shares'], 
+                                                                                                   returns=returns, 
+                                                                                                   yearly=yearly))
